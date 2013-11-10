225f880e8b1cabcef98b9c227e7548dd0dc38d5d
Mike Garrett <mgarrett@webdevelopmentgroup.com>
2013-11-10 14:55:50 -0500

    All fields present in single case template

--- wp-content/themes/rescuePH/single-case.php
+++ wp-content/themes/rescuePH/single-case.php
@@ -36,47 +36,136 @@ single-bookmarks.php
 
 								<section class="entry-content clearfix">
 
-									<h2>Status</h2>
-									<?php echo get_the_term_list( $post->ID, 'status', ' ', ', ', '' ); ?>
-									<h2>Priority</h2>
-									<?php echo get_the_term_list( $post->ID, 'priority', ' ', ', ', '' ); ?>
-									<h2>Type</h2>
-									<?php echo get_the_term_list( $post->ID, 'type', ' ', ', ', '' ); ?>
-									<?php
-									print '<h2>Name</h2>';
-									the_field('name');
-									print '<h2>Contact Info</h2>';
-									the_field('contact_info');
-									print '<h2>Request Source</h2>';
-									the_field('request_source');
-									print '<h2>Location</h2>'; ?>
-									<?php $location = get_field('request_location'); ?>
-									<?php echo $location['address']; ?>
-									<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-									<script>
-									    var map;
-									    var myLatLang = new google.maps.LatLng( <?php echo $location['coordinates']; ?>);
-									    function initialize() {
-									        var mapOptions = {
-									            zoom: 6,
-									            center: myLatLang,
-									            mapTypeId: google.maps.MapTypeId.ROADMAP
-									        };
-									        map = new google.maps.Map(document.getElementById('map-canvas'),
-									        mapOptions);
-									        var marker = new google.maps.Marker({
-									            position: myLatLang,
-									            map: map,
-									            title:"Location"
-									        });
-									    };
-									    google.maps.event.addDomListener(window, 'load', initialize);
-									</script>
-									<div id="map-canvas"></div>
-									<?php if($tracing = get_field('tracing_information')) {
-										print '<h2>Tracing Information</h2>';
-										print $tracing;
-									} ?>
+									<?php if($summary = get_field('summary')) { ?>
+										<h2>Summary</h2>
+										<?php echo $summary; ?>
+									<?php } ?>
+									<?php if($status = get_field('status')) { ?>
+										<h2>Status</h2>
+										<?php
+										echo '<ul>';
+										foreach ($status as $term) {
+										    $term_link = get_term_link( $term, 'status' );
+										    if( is_wp_error( $term_link ) )
+										        continue;
+										    echo '<li><a href="' . $term_link . '">' . $term->name . '</a></li>';
+										}
+										echo '</ul>'; ?>
+									<?php } ?>
+									<?php if($priority = get_field('priority')) { ?>
+										<h2>Priority</h2>
+										<?php
+										echo '<ul>';
+										foreach ($priority as $term) {
+										    $term_link = get_term_link( $term, 'priority' );
+										    if( is_wp_error( $term_link ) )
+										        continue;
+										    echo '<li><a href="' . $term_link . '">' . $term->name . '</a></li>';
+										}
+										echo '</ul>'; ?>
+									<?php } ?>
+									<?php if($type = get_field('type')) { ?>
+										<h2>Type</h2>
+										<?php
+										echo '<ul>';
+										foreach ($type as $term) {
+										    $term_link = get_term_link( $term, 'type' );
+										    if( is_wp_error( $term_link ) )
+										        continue;
+										    echo '<li><a href="' . $term_link . '">' . $term->name . '</a></li>';
+										}
+										echo '</ul>'; ?>
+									<?php } ?>
+
+
+									<h1>Request made by</h1>
+									<?php if($requester_name = get_field('requester_name')) { ?>
+										<h2>Name</h2>
+										<?php echo $requester_name; ?>
+									<?php } ?>
+									<?php if($requester_contact_info = get_field('requester_contact_info')) { ?>
+										<h2>Contact Information</h2>
+										<?php echo $requester_contact_info; ?>
+									<?php } ?>
+									<?php if($request_source = get_field('request_source')) { ?>
+										<h2>Request Source</h2>
+										<?php echo $request_source; ?>
+									<?php } ?>
+
+
+									<? if($type[0]->slug == 'rescue') { ?>
+										<h1>Assistance Request Information</h1>
+										<?php if($name = get_field('name')) { ?>
+											<h2>Name</h2>
+											<?php echo $name; ?>
+										<?php } ?>
+										<?php if($contact = get_field('contact')) { ?>
+											<h2>Contact Information</h2>
+											<?php echo $contact; ?>
+										<?php } ?>
+										<?php if($rescue_location = get_field('request_location')) { ?>
+											<h2>Location</h2>
+											<?php echo $rescue_location['address']; ?>
+											<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+											<script>
+											    var map;
+											    var myLatLang = new google.maps.LatLng( <?php echo $rescue_location['coordinates']; ?>);
+											    function initialize() {
+											        var mapOptions = {
+											            zoom: 6,
+											            center: myLatLang,
+											            mapTypeId: google.maps.MapTypeId.ROADMAP
+											        };
+											        map = new google.maps.Map(document.getElementById('map-canvas'),
+											        mapOptions);
+											        var marker = new google.maps.Marker({
+											            position: myLatLang,
+											            map: map,
+											            title:"Rescue Location"
+											        });
+											    };
+											    google.maps.event.addDomListener(window, 'load', initialize);
+											</script>
+											<div id="map-canvas"></div>
+										<?php } // End Rescue location ?>
+									<?php } // End check for rescue type ?>
+									<? if($type[0]->slug == 'tracing') { ?>
+										<h1>Tracing Request Information</h1>
+										<?php if($tracing_name = get_field('tracing_name')) { ?>
+											<h2>Name of Missing Person</h2>
+											<?php echo $tracing_name; ?>
+										<?php } ?>
+										<?php if($tracing_location = get_field('tracing_location')) { ?>
+											<h2>Last Seen</h2>
+											<?php echo $tracing_location['address']; ?>
+											<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+											<script>
+											    var map;
+											    var myLatLang = new google.maps.LatLng( <?php echo $tracing_location['coordinates']; ?>);
+											    function initialize() {
+											        var mapOptions = {
+											            zoom: 6,
+											            center: myLatLang,
+											            mapTypeId: google.maps.MapTypeId.ROADMAP
+											        };
+											        map = new google.maps.Map(document.getElementById('map-canvas'),
+											        mapOptions);
+											        var marker = new google.maps.Marker({
+											            position: myLatLang,
+											            map: map,
+											            title:"Last Seen Location"
+											        });
+											    };
+											    google.maps.event.addDomListener(window, 'load', initialize);
+											</script>
+											<div id="map-canvas"></div>
+										<?php } // End Tracing location ?>
+										<?php if($tracing_information = get_field('tracing_information')) { ?>
+											<h2>Additional Information</h2>
+											<?php echo $tracing_information; ?>
+										<?php } ?>
+									<?php } // End check for tracing type ?>
+
 									<h2>More details</h2>
 									<?php the_content(); ?>
 
