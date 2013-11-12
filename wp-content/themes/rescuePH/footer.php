6643ba3e7dc591940fdbc385f2b4cb1e169eb3f3
Mike Garrett <mgarrett@webdevelopmentgroup.com>
2013-11-11 23:40:35 -0500

    Footer attribution

--- wp-content/themes/rescuePH/footer.php
+++ wp-content/themes/rescuePH/footer.php
@@ -6,7 +6,7 @@
 							<?php bones_footer_links(); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Built at <a href="https://geekli.st/hackathon/52793a2660fb3f52d50001f8">Geeklist #hack4good 0.4</a></p>
 
 				</div>
 
@@ -16,18 +16,18 @@
 
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
-			
-		
+
+
 		<!-- DataTables CSS -->
 		<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
-		 		
+
 		<!-- jQuery -->
 		<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
 
 		<!-- DataTables -->
 		<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
 		<script type="text/javascript" charset="utf8">
-		
+
 		$(document).ready(function(){
 		  $('#caseTable').dataTable();
 		});
